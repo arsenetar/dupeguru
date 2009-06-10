73ac65fa4eaaee4b5125eb6865930bcf8f8d45e7
hsoft <none@none>
2009-06-10 11:17:43 +0000

    se cocoa: v2.7.2
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%4033

--- se/cocoa/py/gen.py
+++ se/cocoa/py/gen.py
@@ -4,12 +4,15 @@ import os
 import os.path as op
 import shutil
 
-print "Cleaning build and dist"
+from hsutil.build import print_and_do
+
+os.chdir('dupeguru')
+print_and_do('python gen.py')
+os.chdir('..')
+
 if op.exists('build'):
     shutil.rmtree('build')
 if op.exists('dist'):
     shutil.rmtree('dist')
 
-print "Buiding the py2app plugin"
-
-os.system('python -u setup.py py2app')
+print_and_do('python -u setup.py py2app')
