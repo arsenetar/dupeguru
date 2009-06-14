8ce12d785f427ec95bc24aa5e1c8119cbb20b23a
hsoft <none@none>
2009-06-14 15:22:03 +0000

    me qt: v5.6.2 (forgot to commit)
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%4047

--- me/qt/app.py
+++ me/qt/app.py
@@ -17,7 +17,7 @@ from preferences_dialog import PreferencesDialog
 class DupeGuru(DupeGuruBase):
     LOGO_NAME = 'logo_me'
     NAME = 'dupeGuru Music Edition'
-    VERSION = '5.6.1'
+    VERSION = '5.6.2'
     DELTA_COLUMNS = frozenset([2, 3, 4, 5, 7, 8])
     
     def __init__(self):
