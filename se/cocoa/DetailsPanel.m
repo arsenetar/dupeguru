1bffe0361621535c37c1f6fe24535947b664156d
hsoft <none@none>
2009-06-15 12:34:32 +0000

    se cocoa: pushed DetailsPanel common code down to dgbase.
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%4054

--- se/cocoa/DetailsPanel.m
+++ se/cocoa/DetailsPanel.m
@@ -1,16 +1,4 @@
 #import "DetailsPanel.h"
 
 @implementation DetailsPanel
-- (id)initWithPy:(PyApp *)aPy
-{
-    self = [super initWithWindowNibName:@"Details"];
-    [self window]; //So the detailsTable is initialized.
-    [detailsTable setPy:aPy];
-    return self;
-}
-
-- (void)refresh
-{
-    [detailsTable reloadData];
-}
 @end
