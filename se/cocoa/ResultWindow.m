f67d7ce1e5963f990199925e3384604122ad214d
hsoft <none@none>
2009-06-15 12:55:59 +0000

    se cocoa: Removed the details panel refresh mechanism because it is now handled in dgbase.
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%4058

--- se/cocoa/ResultWindow.m
+++ se/cocoa/ResultWindow.m
@@ -24,7 +24,6 @@
     [self initResultColumns];
     [self refreshStats];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(resultsMarkingChanged:) name:ResultsMarkingChangedNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(duplicateSelectionChanged:) name:DuplicateSelectionChangedNotification object:nil];
     
     NSToolbar *t = [[[NSToolbar alloc] initWithIdentifier:@"ResultWindowToolbar"] autorelease];
     [t setAllowsUserCustomization:YES];
@@ -387,12 +386,6 @@
 }
 
 /* Notifications */
-- (void)duplicateSelectionChanged:(NSNotification *)aNotification
-{
-    if (_detailsPanel)
-        [_detailsPanel refresh];
-}
-
 - (void)outlineViewSelectionDidChange:(NSNotification *)notification
 {
     [self performPySelection:[self getSelectedPaths:NO]];
