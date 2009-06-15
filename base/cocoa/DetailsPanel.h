308ab0cd529e684449c0c602fae143c70c8788ae
hsoft <none@none>
2009-06-15 12:33:58 +0000

    base cocoa: pushed DetailsPanel common code down.
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%4053

---
+++ base/cocoa/DetailsPanel.h
@@ -0,0 +1,13 @@
+#import <Cocoa/Cocoa.h>
+#import "PyApp.h"
+#import "Table.h"
+
+
+@interface DetailsPanelBase : NSWindowController
+{
+    IBOutlet TableView *detailsTable;
+}
+- (id)initWithPy:(PyApp *)aPy;
+
+- (void)refresh;
+@end
