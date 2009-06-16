3deba9459805bb01dada4ec0e717bbd11ba5dbab
hsoft <none@none>
2009-06-16 08:09:42 +0000

    [#3] me cocoa: Pushed power marker related code down to base.
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%4063

--- me/cocoa/ResultWindow.m
+++ me/cocoa/ResultWindow.m
@@ -41,18 +41,6 @@
 }
 
 /* Actions */
-
-- (IBAction)changePowerMarker:(id)sender
-{
-    _powerMode = [pmSwitch selectedSegment] == 1;
-    if (_powerMode)
-        [matches setTag:2];
-    else
-        [matches setTag:0];
-    [self expandAll:nil];
-    [self outlineView:matches didClickTableColumn:nil];
-}
-
 - (IBAction)clearIgnoreList:(id)sender
 {
     int i = n2i([py getIgnoreListCount]);
@@ -268,15 +256,6 @@
         [[_detailsPanel window] close];
     else
         [[_detailsPanel window] orderFront:nil];
-}
-
-- (IBAction)togglePowerMarker:(id)sender
-{
-    if ([pmSwitch selectedSegment] == 1)
-        [pmSwitch setSelectedSegment:0];
-    else
-        [pmSwitch setSelectedSegment:1];
-    [self changePowerMarker:sender];
 }
 
 /* Public */
