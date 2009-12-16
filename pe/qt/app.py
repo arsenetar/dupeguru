51b14435e05ce00b71e51c704565b5ea37b8af13
hsoft <none@none>
2009-12-16 09:54:00 +0000

    dgpe qt v1.8.0
    --HG--
    extra : convert_revision : svn%3Ac306627e-7827-47d3-bdf0-9a457c9553a1/trunk%40259

--- pe/qt/app.py
+++ pe/qt/app.py
@@ -57,7 +57,7 @@ class File(fs.File):
 class DupeGuru(DupeGuruBase):
     LOGO_NAME = 'logo_pe'
     NAME = 'dupeGuru Picture Edition'
-    VERSION = '1.7.7'
+    VERSION = '1.8.0'
     DELTA_COLUMNS = frozenset([2, 5, 6])
     
     def __init__(self):
