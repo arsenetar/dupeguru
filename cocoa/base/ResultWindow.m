c14fecb41567dd193d7112c54a194d3e06c31bbc
Virgil Dupras <hsoft@hardcoded.net>
2010-01-01 21:11:34 +0100

    Changed copyright year to 2010

--- cocoa/base/ResultWindow.m
+++ cocoa/base/ResultWindow.m
@@ -1,5 +1,5 @@
 /* 
-Copyright 2009 Hardcoded Software (http://www.hardcoded.net)
+Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
 
 This software is licensed under the "HS" License as described in the "LICENSE" file, 
 which should be included with this package. The terms are also available at 
