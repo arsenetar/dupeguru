47b38c7d455da95d67d8869d15ed11ef419e8e27
Virgil Dupras <hsoft@hardcoded.net>
2010-02-13 12:22:34 -0800

    Preliminary linux support (it starts up, at least...).

--- qt/base/platform_lnx.py
+++ qt/base/platform_lnx.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 # Created By: Virgil Dupras
-# Created On: 2009-09-27
+# Created On: 2010-02-13
 # Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
 # 
 # This software is licensed under the "HS" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
-import logging
-import sys
+INITIAL_FOLDER_IN_DIALOGS = '/'
 
-if sys.platform == 'win32':
-    from platform_win import *
-elif sys.platform == 'darwin':
-    from platform_osx import *
-elif sys.platform == 'linux2':
-    from platform_lnx import *
-else:
-    pass # unsupported platform
+def recycle_file(path):
+    pass
