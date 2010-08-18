939efd7dab6334568d3851547a28b8678687904e
Virgil Dupras <hsoft@hardcoded.net>
2010-08-18 11:37:18 +0200

    Updated py2app workarounds for ME.

--- cocoa/me/dg_cocoa.py
+++ cocoa/me/dg_cocoa.py
@@ -11,10 +11,17 @@ from core_me.app_cocoa import DupeGuruME
 from core.scanner import ScanType
 
 # Fix py2app imports which chokes on relative imports and other stuff
-from core_me import app_cocoa, data, fs, scanner
+import core_me.app_cocoa, core_me.data, core_me.fs, core_me.scanner
+import hsaudiotag.aiff, hsaudiotag.flac, hsaudiotag.genres, hsaudiotag.id3v1,\
+    hsaudiotag.id3v2, hsaudiotag.mp4, hsaudiotag.mpeg, hsaudiotag.ogg, hsaudiotag.wma
 from hsaudiotag import aiff, flac, genres, id3v1, id3v2, mp4, mpeg, ogg, wma
+import hsutil.conflict
+import core.engine, core.fs, core.app
+import core_se.fs, core_se.data
 import xml.etree.ElementPath
 import gzip
+import aem.kae
+import appscript.defaultterminology
 
 class PyDupeGuru(PyDupeGuruBase):
     def init(self):
