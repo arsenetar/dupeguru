4517bea6640c2cdeda263e05c3bcf9e82a7e43ba
Virgil Dupras <hsoft@hardcoded.net>
2010-10-05 10:22:02 +0200

    Moved the start.py file directly in qt run template instead of using this subprocess thingy. Much easier for packaging.

--- run_template_qt.py
+++ run_template_qt.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
+# Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
+# 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
+# which should be included with this package. The terms are also available at 
+# http://www.hardcoded.net/licenses/bsd_license
 
 import sys
-import os
-import os.path as op
-import subprocess
+import sip
+sip.setapi('QVariant', 1)
 
-def main():
-    scriptpath = op.abspath(__file__)
-    scriptfolder = op.dirname(scriptpath)
-    os.environ['PYTHONPATH'] = scriptfolder
-    subprocess.Popen([sys.executable, '-m', 'qt.{{edition}}.start'], env=os.environ)
+from PyQt4.QtCore import QCoreApplication
+from PyQt4.QtGui import QApplication, QIcon, QPixmap
 
-if __name__ == '__main__':
-    sys.exit(main())
+from qt.base import dg_rc
+from qt.{{edition}}.app import DupeGuru
+
+if sys.platform == 'win32':
+    import base.cxfreeze_fix
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(QPixmap(":/{0}".format(DupeGuru.LOGO_NAME))))
+    QCoreApplication.setOrganizationName('Hardcoded Software')
+    QCoreApplication.setApplicationName(DupeGuru.NAME)
+    QCoreApplication.setApplicationVersion(DupeGuru.VERSION)
+    dgapp = DupeGuru()
+    sys.exit(app.exec_())
