8a8a1811865507a1112804225fd5afac28a0fda3
Virgil Dupras <hsoft@hardcoded.net>
2010-10-05 03:50:28 -0700

    se 2.12.2

--- qt/se/app.py
+++ qt/se/app.py
@@ -27,7 +27,7 @@ class DupeGuru(DupeGuruBase):
     EDITION = 'se'
     LOGO_NAME = 'logo_se'
     NAME = 'dupeGuru'
-    VERSION = '2.12.1'
+    VERSION = '2.12.2'
     DELTA_COLUMNS = frozenset([2, 4])
     
     def __init__(self):
