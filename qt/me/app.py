7ddf9772df5f734575a0eb499289e022a85dadad
Virgil Dupras <hsoft@hardcoded.net>
2010-11-21 16:25:16 +0100

    v5.10.3

--- qt/me/app.py
+++ qt/me/app.py
@@ -17,7 +17,7 @@ class DupeGuru(DupeGuruBase):
     EDITION = 'me'
     LOGO_NAME = 'logo_me'
     NAME = 'dupeGuru Music Edition'
-    VERSION = '5.10.2'
+    VERSION = '5.10.3'
     DELTA_COLUMNS = frozenset([2, 3, 4, 5, 7])
     
     def __init__(self):
