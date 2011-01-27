7c38217308533b6e03315d3335399a227c9d5b3a
Virgil Dupras <hsoft@hardcoded.net>
2011-01-27 02:22:10 -0800

    Fixed pref dialog UI for Linux.

--- qt/se/preferences_dialog.py
+++ qt/se/preferences_dialog.py
@@ -87,7 +87,7 @@ class PreferencesDialog(PreferencesDialogBase):
         if sys.platform == 'linux2':
             # Under linux, whether it's a Qt layout bug or something else, the size threshold text edit
             # doesn't have enough space, so we make the pref pane higher to compensate.
-            self.resize(self.width(), 480)
+            self.resize(self.width(), 490)
         elif sys.platform == 'win32':
             self.resize(self.width(), 420)
     
