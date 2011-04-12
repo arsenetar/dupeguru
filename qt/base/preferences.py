0fea59007caff85275e529671e49fda17dd0a9ea
Virgil Dupras <hsoft@hardcoded.net>
2011-04-12 10:04:01 +0200

    Updated copyright year to 2011.

--- qt/base/preferences.py
+++ qt/base/preferences.py
@@ -1,6 +1,6 @@
 # Created By: Virgil Dupras
 # Created On: 2009-05-03
-# Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
+# Copyright 2011 Hardcoded Software (http://www.hardcoded.net)
 # 
 # This software is licensed under the "BSD" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
