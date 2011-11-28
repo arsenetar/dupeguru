69fbda5d2cf84b78288a4897b58d3f86ca24280c
Virgil Dupras <hsoft@hardcoded.net>
2011-11-28 14:07:09 -0500

    Added qt.(se|me|pe).result_model which I had forgot to add in previous commit.

---
+++ qt/me/results_model.py
@@ -0,0 +1,35 @@
+# Created On: 2011-11-27
+# Copyright 2011 Hardcoded Software (http://www.hardcoded.net)
+# 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
+# which should be included with this package. The terms are also available at 
+# http://www.hardcoded.net/licenses/bsd_license
+
+from qtlib.column import Column
+from ..base.results_model import ResultsModel as ResultsModelBase
+from core_se.result_table import ResultTable
+
+# Little hack to remove the 'marked' column which isn't there in the Qt GUI.
+del ResultTable.COLUMNS[0]
+
+class ResultsModel(ResultsModelBase):
+    COLUMNS = [
+        Column('name', coltr("Filename")),
+        Column('folder_path', coltr("Folder"), visible=False, optional=True),
+        Column('size', coltr("Size (MB)"), optional=True),
+        Column('duration', coltr("Time"), optional=True),
+        Column('bitrate', coltr("Bitrate"), optional=True),
+        Column('samplerate', coltr("Sample Rate"), visible=False, optional=True),
+        Column('extension', coltr("Kind"), optional=True),
+        Column('mtime', coltr("Modification"), visible=False, optional=True),
+        Column('title', coltr("Title"), visible=False, optional=True),
+        Column('artist', coltr("Artist"), visible=False, optional=True),
+        Column('album', coltr("Album"), visible=False, optional=True),
+        Column('genre', coltr("Genre"), visible=False, optional=True),
+        Column('year', coltr("Year"), visible=False, optional=True),
+        Column('track', coltr("Track Number"), visible=False, optional=True),
+        Column('comment', coltr("Comment"), visible=False, optional=True),
+        Column('percentage', coltr("Match %"), optional=True),
+        Column('words', coltr("Words Used"), visible=False, optional=True),
+        Column('dupe_count', coltr("Dupe Count"), visible=False, optional=True),
+    ]
---
+++ qt/se/results_model.py
@@ -0,0 +1,25 @@
+# Created On: 2011-11-27
+# Copyright 2011 Hardcoded Software (http://www.hardcoded.net)
+# 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
+# which should be included with this package. The terms are also available at 
+# http://www.hardcoded.net/licenses/bsd_license
+
+from qtlib.column import Column
+from ..base.results_model import ResultsModel as ResultsModelBase
+from core_se.result_table import ResultTable
+
+# Little hack to remove the 'marked' column which isn't there in the Qt GUI.
+del ResultTable.COLUMNS[0]
+
+class ResultsModel(ResultsModelBase):
+    COLUMNS = [
+        Column('name',defaultWidth=200),
+        Column('folder_path',defaultWidth=180),
+        Column('size',defaultWidth=60),
+        Column('extension',defaultWidth=40),
+        Column('mtime',defaultWidth=120),
+        Column('percentage',defaultWidth=60),
+        Column('words',defaultWidth=120),
+        Column('dupe_count',defaultWidth=80),
+    ]
