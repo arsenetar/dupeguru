6d263215ad21cd500178c7ced72d02ae93b7ca6c
Virgil Dupras <hsoft@hardcoded.net>
2015-04-05 09:17:35 -0400

    Fix wrong use_regexp option propagation to core (qt)
    We need to flip `use_regexp` before sending it down to
    `escape_filter_regexp`!
    
    fixes #295

--- qt/base/app.py
+++ qt/base/app.py
@@ -90,7 +90,7 @@ class DupeGuru(QObject):
 
     def _update_options(self):
         self.model.scanner.mix_file_kind = self.prefs.mix_file_kind
-        self.model.options['escape_filter_regexp'] = self.prefs.use_regexp
+        self.model.options['escape_filter_regexp'] = not self.prefs.use_regexp
         self.model.options['clean_empty_dirs'] = self.prefs.remove_empty_folders
         self.model.options['ignore_hardlink_matches'] = self.prefs.ignore_hardlink_matches
         self.model.options['copymove_dest_type'] = self.prefs.destination_type
