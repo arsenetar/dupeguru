abd2f3a9d65fb1bd1f6d2318c45426affcc33cd4
Virgil Dupras <hsoft@hardcoded.net>
2016-05-29 17:08:54 -0400

    core_pe: fix missing scanner option refactoring

--- core_pe/app.py
+++ core_pe/app.py
@@ -1,8 +1,7 @@
-# Created On: 2011/09/20
-# Copyright 2015 Hardcoded Software (http://www.hardcoded.net)
-# 
-# This software is licensed under the "GPLv3" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+# Copyright 2016 Hardcoded Software (http://www.hardcoded.net)
+#
+# This software is licensed under the "GPLv3" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.gnu.org/licenses/gpl-3.0.html
 
 import os.path as op
@@ -18,11 +17,11 @@ class DupeGuru(DupeGuruBase):
     NAME = __appname__
     METADATA_TO_READ = ['size', 'mtime', 'dimensions', 'exif_timestamp']
     SCANNER_CLASS = ScannerPE
-    
+
     def __init__(self, view):
         DupeGuruBase.__init__(self, view)
-        self.scanner.cache_path = op.join(self.appdata, 'cached_pictures.db')
-    
+        self.options['cache_path'] = op.join(self.appdata, 'cached_pictures.db')
+
     def _get_dupe_sort_key(self, dupe, get_group, key, delta):
         if key == 'folder_path':
             dupe_folder_path = getattr(dupe, 'display_folder_path', dupe.folder_path)
@@ -32,15 +31,15 @@ class DupeGuru(DupeGuruBase):
             ref_value = cmp_value(get_group().ref, key)
             return get_delta_dimensions(r, ref_value)
         return DupeGuruBase._get_dupe_sort_key(self, dupe, get_group, key, delta)
-    
+
     def _get_group_sort_key(self, group, key):
         if key == 'folder_path':
             dupe_folder_path = getattr(group.ref, 'display_folder_path', group.ref.folder_path)
             return str(dupe_folder_path).lower()
         return DupeGuruBase._get_group_sort_key(self, group, key)
-    
+
     def _prioritization_categories(self):
         return prioritize.all_categories()
-    
+
     def _create_result_table(self):
         return ResultTable(self)
