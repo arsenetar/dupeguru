4b1c925ab1176e18fb82aa63163d6d84827fe3ca
glubsy <glubsy@users.noreply.github.com>
2020-05-07 16:24:07 +0200

    use a QKeySequence instead

--- qt/result_window.py
+++ qt/result_window.py
@@ -169,7 +169,7 @@ class ResultWindow(QMainWindow):
             ),
             (
                 "actionMarkSelected",
-                "Space",
+                Qt.Key_Space,
                 "",
                 tr("Mark Selected"),
                 self.markSelectedTriggered,
