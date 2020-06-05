b048fa59680048f927720b8cde014fb068bd8e10
Joe Moore <jpvlsmv@gmail.com>
2020-06-05 19:44:08 -0400

    Rename an ell variable into something that flake8 doesn't complain about

--- hscommon/build.py
+++ hscommon/build.py
@@ -557,7 +557,7 @@ def fix_qt_resource_file(path):
     with open(path, "rb") as fp:
         contents = fp.read()
     lines = contents.split(b"\n")
-    lines = [l for l in lines if not l.startswith(b"#")]
+    lines = [lin for lin in lines if not lin.startswith(b"#")]
     with open(path, "wb") as fp:
         fp.write(b"\n".join(lines))
 
